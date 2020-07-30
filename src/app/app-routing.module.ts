81ec1b923bbbced9bf5ec4fb9da0f4d88dc83210
idang.dev <idang.dev@gmail.com>
2020-07-30 11:30:52 +0700

    fix ganti password

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tab/tab.module').then(m => m.TabPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'regist',
+    loadChildren: () => import('./regist/regist.module').then( m => m.RegistPageModule)
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)
+  },
+  {
+    path: 'tentang',
+    loadChildren: () => import('./tentang/tentang.module').then( m => m.TentangPageModule)
+  }
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
